b74122b9821563fc1fce47508bd978190b379132
Diego Nieto <diego.nieto@diegonieto.org>
2020-03-29 17:43:02 +0200

    Adding Week2-Ex2 cost function and predict

--- week3/machine-learning-ex2/ex2/costFunction.m
+++ week3/machine-learning-ex2/ex2/costFunction.m
@@ -8,24 +8,14 @@ function [J, grad] = costFunction(theta, X, y)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
+part1 = -y.*log(sigmoid(X*theta));       % y == 1 => low cost
+part2 = (1-y).*log(1-sigmoid(X*theta)); % y == 0 => high cost
 
+J = sum(part1-part2)/m;
 
-
-
-
-
-
+grad = 1/m * X'*(sigmoid(X*theta)-y)
 
 % =============================================================
 
