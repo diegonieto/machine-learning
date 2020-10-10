56517a5a542ff5a4d53770d292bee69d24497bc4
Diego Nieto <diego.nieto@diegonieto.org>
2020-10-11 00:39:10 +0200

    addingregularization in collaborative filtering gradient for cost and gradient

--- week9/machine-learning-ex8/ex8/cofiCostFunc.m
+++ week9/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,14 +40,14 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-
-
+% cost function
 errors = ((X*Theta'-Y).*R);
 squaredErrors = errors.^2;
-J = ((1/2)*sum(squaredErrors(:)));
+J = ((1/2)*sum(squaredErrors(:)))+((lambda/2)*sum(Theta(:).^ 2))+((lambda/2)*sum(X(:).^2));
 
-X_grad = errors*Theta;
-Theta_grad = errors'*X;
+% gredient
+X_grad = errors*Theta.+(lambda.*X);
+Theta_grad = errors'*X.+(lambda.*Theta);
 
 % =============================================================
 
